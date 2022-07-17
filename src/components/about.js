ed2dcca6f08d9f15873bc8be48bb4ccbff9fd644
Ben Liu <ben196888@gmail.com>
2022-07-18 00:51:00 +0700

    fix: remove non-cms content

--- src/components/about.js
+++ src/components/about.js
@@ -14,9 +14,9 @@ const Header = ({ data }) => (
         </div>
         <div className="left col-md-7 col-lg-8">
           <div className="about-details">
-            <span className="name">My Name is {data.name}.</span>
+            <span className="name">{data.name}</span>
             <h2 className="sub-position">
-              I'm a Professional {data.designation}.
+              {data.designation}
             </h2>
             {
               data.description &&
